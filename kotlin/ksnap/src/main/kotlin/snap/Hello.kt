4dc0c1ff280afe44a5fc9d5900f9b459c529a6d6
Curt Cox <curtcox@gmail.com>
2022-09-12 09:55:48 -0500

    Added Kotlin generated from archetype:
    mvn archetype:generate -Dfilter=org.jetbrains.kotlin:

---
+++ kotlin/ksnap/src/main/kotlin/snap/Hello.kt
@@ -0,0 +1,6 @@
+package snap
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ kotlin/ksnap/src/test/kotlin/snap/HelloTest.kt
@@ -0,0 +1,12 @@
+package snap
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+    @Test
+    fun testSum() {
+        assertEquals(42, 6 * 7)
+    }
+}
