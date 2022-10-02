cdec303c4de9aa2eb9170332f4dfce401b9c3fe1
Curt Cox <curtcox@gmail.com>
2022-10-02 15:51:08 -0500

    WIP : Adding tests.

--- src/main/java/com/curtcox/snap/connectors/StreamIO.java
+++ src/main/java/com/curtcox/snap/connectors/StreamIO.java
@@ -3,6 +3,9 @@ package com.curtcox.snap.connectors;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * A paired input stream and output stream.
+ */
 public final class StreamIO {
 
     public final InputStream in;
--- src/test/java/com/curtcox/snap/connectors/PacketStreamBridgeTest.java
+++ src/test/java/com/curtcox/snap/connectors/PacketStreamBridgeTest.java
@@ -1,24 +1,45 @@
 package com.curtcox.snap.connectors;
 
+import com.curtcox.snap.model.PacketReaderWriter;
+import com.curtcox.snap.model.Random;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
+import java.io.*;
+
+import com.curtcox.snap.model.Packet;
+import static com.curtcox.snap.model.Packet.*;
 import static org.junit.Assert.*;
 
 public class PacketStreamBridgeTest {
 
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(2);
+
+    PacketStreamBridge bridge = new PacketStreamBridge();
     @Test
     public void can_create() {
         assertNotNull(new PacketStreamBridge());
     }
 
     @Test
-    public void read_returns_null_when_there_are_no_streams() {
-        fail();
+    public void read_returns_null_when_there_are_no_streams() throws IOException {
+        assertNull(bridge.read(ANY));
     }
 
     @Test
-    public void read_returns_packet_from_1_stream() {
-        fail();
+    public void read_returns_packet_from_1_stream() throws IOException {
+        Packet packet = Random.packet();
+        InputStream in = new ByteArrayInputStream(new byte[0]);
+        OutputStream out = new ByteArrayOutputStream();
+        StreamIO streamIO = new StreamIO(in,out);
+        PacketReaderWriter readerWriter = PacketReaderWriter.from(streamIO);
+        bridge.accept(streamIO);
+        readerWriter.write(packet);
+
+        Packet read = bridge.read(ANY);
+        assertEquals(packet,read);
     }
 
     @Test
